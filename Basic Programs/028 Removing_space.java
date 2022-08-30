14b43a9a582b90961794761c1338d73c1bd3eaea
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2022-08-30 21:20:07 +0530

    Create 028 Removing_space.java

---
+++ Basic Programs/028 Removing_space.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      String s = "$$$$%@112  a%% $#44ff fv@!!";
+      s = s.replaceAll("\\s","");
+      System.out.println(s);
+  }
+}
+
+//Output
+$$$$%@112a%%$#44fffv@!!
