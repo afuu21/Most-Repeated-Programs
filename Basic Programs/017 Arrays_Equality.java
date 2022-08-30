5efdda2826e8101bcd39a9b95aa9680ad433407b
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2022-08-30 18:36:28 +0530

    Update 017 Arrays_Equality.java

--- Basic Programs/017 Arrays_Equality.java
+++ Basic Programs/017 Arrays_Equality.java
@@ -13,3 +13,27 @@ public class Main {
 
 //Output
 Arrays are not equal!
+    
+//Alternate Method
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a1[] = {3,4,5,6,7};
+      int a2[] = {3,4,5,6,7,5,6};
+      if(a1.length != a2.length)
+        System.out.print("Arrays are not equal!");
+      else {
+        for(int i=0; i<a1.length; i++) {
+          if(a1[i]!=a2[i]) {
+            System.out.println("Arrays are not equal!");
+            return;
+          }
+        }
+        System.out.println("Arrays are equal!");
+      }
+  }
+}
+
+//Output
+Arrays are not equal!
