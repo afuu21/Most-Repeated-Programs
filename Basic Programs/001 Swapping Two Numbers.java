c550d31e429e47cc830b3904d9bd4c8dc0b22b3f
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2022-08-30 11:35:31 +0530

    Update 001 Swapping Two Numbers.java

--- Basic Programs/001 Swapping Two Numbers.java
+++ Basic Programs/001 Swapping Two Numbers.java
@@ -16,4 +16,76 @@ class HelloWorld {
 Before Swapping 10 20
 After Swapping 20 10
 
-//Method 2: 
+//Method 2: Using + and -
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 20;
+        System.out.println("Before Swapping "+a+" "+b);
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("After Swapping "+a+" "+b);
+    }
+}
+
+//Output
+Before Swapping 10 20
+After Swapping 20 10
+
+//Method 3: Using * and / NOTE: a and b cannot be 0 in this case
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 20;
+        System.out.println("Before Swapping "+a+" "+b);
+        a = a*b;
+        b = a/b;
+        a = a/b;
+        System.out.println("After Swapping "+a+" "+b);
+    }
+}
+
+//Output
+Before Swapping 10 20
+After Swapping 20 10
+    
+//Method 4: Using XOR operator The logic is that a^a=0
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 20;
+        System.out.println("Before Swapping "+a+" "+b);
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println("After Swapping "+a+" "+b);
+    }
+}
+
+//Output
+Before Swapping 10 20
+After Swapping 20 10
+
+//Method 5: Single statement
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 20;
+        System.out.println("Before Swapping "+a+" "+b);
+        b = a+b-(a=b);
+        System.out.println("After Swapping "+a+" "+b);
+    }
+}
+
+//Output
+Before Swapping 10 20
+After Swapping 20 10
