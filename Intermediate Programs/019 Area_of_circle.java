be21c8d92b5c19de0f3575bed61726dd333de124
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2022-09-07 12:52:34 +0530

    Create 019 Area_of_circle.java

---
+++ Intermediate Programs/019 Area_of_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the radius: ");
+      int r = sc.nextInt();
+      System.out.println("Ans: " + 3.14*r*r);
+    }
+}
+//Output
+3
+Ans: 28.259999999999998
